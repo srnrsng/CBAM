a21cb9b874bd86d0858ac9422dfc34b3c6c089c5
lenovo <lenovo@RANJAN>
2025-12-08 12:02:16 +0530

    Add project files.

---
+++ CBAM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CBAM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
